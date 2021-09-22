d4815542961278e8ade4f439d9755860b4ff85b0
Femastia <khairul.ihsan12499@gmail.com>
2021-09-22 16:18:14 +0700

    tugas ch4

---
+++ app/src/main/java/com/ihsan/layoutinstagram/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ihsan.layoutinstagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.widget.TextView
+import androidx.core.text.bold
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
